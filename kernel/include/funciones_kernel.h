fd3e6296a7621db00b4685b7ec91e1f47ff53ae2
Franco Pasqualino <fpasqualino@frba.utn.edu.ar>
2022-04-30 02:59:56 -0300

    Agrego estr. de PCB y func. "estimar_rafaga"

--- kernel/include/funciones_kernel.h
+++ kernel/include/funciones_kernel.h
@@ -18,7 +18,28 @@
 	uint32_t parametro2;
 }instrucciones;*/
 
+//TODO: chequear los tipos de datos
+typedef struct{
+	char* id;
+	uint32_t tamanio;
+	instrucciones* list_instr;
+	int prog_counter;
+    pagina tab_pag;
+    rafaga_t rafaga; 
+}PCB_t;
+
+//TODO: chequear los tipos de datos
+typedef struct{
+	int* rafaga_actual;
+	int* duracion_total;
+	int* rafaga_anterior;
+}rafaga_t;
+
+typedef struct{}pagina;
+
 
 void cargar_instruccion(int id, char* nombre, uint32_t parametro1, uint32_t parametro2);
+
+int estimar_rafaga(rafaga_t rafaga, double alfa);
 
 #endif /* KERNEL_INCLUDE_FUNCIONES_KERNEL_H_ */
