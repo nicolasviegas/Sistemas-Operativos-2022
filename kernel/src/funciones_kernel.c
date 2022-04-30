14724b35121341c0f29d5428ed09da917b6b3a80
Franco Pasqualino <fpasqualino@frba.utn.edu.ar>
2022-04-30 03:38:29 -0300

    Revierto commit anterior accidental

--- kernel/src/funciones_kernel.c
+++ kernel/src/funciones_kernel.c
@@ -12,18 +12,7 @@ void cargar_instruccion(int id, char* nombre, uint32_t parametro1, uint32_t para
 	list_add(lista_instrucciones_kernel,estructura_instrucciones);
 }
 
-//TODO: revisar funcionamiento de esta funcion
 
-int estimar_rafaga(rafaga_t rafaga, double alfa){
-	int* nueva_rafaga;
-    nueva_rafaga = (rafaga.duracion_total * alfa) + (1 - alfa) * rafaga.rafaga_anterior;
-
-	(*nueva_rafaga) = rafaga.rafaga_actual;
-	(*rafaga.rafaga_anterior) = rafaga.rafaga_actual;
-	(*rafaga.duracion_total) = rafaga.duracion_total + nueva_rafaga;
-	
-	return rafaga;
-}
 
 
 
