ddf86f636ee8dba4b595c13d00be88740c70c02f
nicolasviegas <nicolasviegas38@gmail.com>
2022-06-05 19:59:14 -0300

    Makefile funcionando para actualizar las shared

---
+++ Release/cpu/Release/src/subdir.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../cpu/Release/src/comunicacion_cpu.o \
+../cpu/Release/src/funciones_cpu.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ shared/Release/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/protocolo.c \
+../src/socket.c \
+../src/utils.c 
+
+OBJS += \
+./src/protocolo.o \
+./src/socket.o \
+./src/utils.o 
+
+C_DEPS += \
+./src/protocolo.d \
+./src/socket.d \
+./src/utils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
